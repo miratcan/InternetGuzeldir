fa3bff3aae4cdebbdae5eebb8cc9f5c32b56bdd7
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2020-10-04 22:35:24 +0300

    IDK

--- render.py
+++ render.py
@@ -1,11 +1,38 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-import json
-env = Environment(loader=FileSystemLoader('templates/'), 
-                  autoescape=select_autoescape(['html', 'xml']))
+import urllib.request
+import csv
+import codecs
+from collections import defaultdict
 
+URL = "https://docs.google.com/spreadsheets/u/3/d/" \
+          "1mK5BycfvwvuPcekTKIMhPKtsRa0EXe-dGeQsvok5wz4/pub?output=csv"
+
+env = Environment(
+    loader=FileSystemLoader('templates/'),
+    autoescape=select_autoescape(['html', 'xml']))
+
+stream = urllib.request.urlopen(URL)
+csv_file = csv.reader(codecs.iterdecode(stream, 'utf-8'))
+
+next(csv_file)
+
+sections = defaultdict(dict)
+
+for line in csv_file:
+    title, url, desc, category, tayp, lang = \
+        line
+    sections[category][title] = {
+        'url': url,
+        'desc': desc,
+        'tayp': tayp,
+        'lang': lang
+    }
+
+
+"""
 with open('sections.json', 'r') as file:
     sections = json.load(file)
-
+"""
 home = env.get_template('home.html.jinja2')
 
 with open('docs/index.html', 'w')as file:
