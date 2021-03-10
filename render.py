01de4dd67d2dbbb482bf4e0a4edb777d382f1ad4
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2021-03-10 09:53:51 +0300

    ...

--- render.py
+++ render.py
@@ -3,9 +3,11 @@ import urllib.request
 from slugify import slugify
 import datetime
 from os.path import join
+from os import makedirs
+import pprint
 
 
-DEFAULT_CATEGORY_SEPARATOR = ">"
+CATEGORY_SEPARATOR = ">"
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -14,58 +16,56 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml']))
 
 
-def get_from_parts(_dict, parts):
-    elem = _dict
-    for part in parts:
-        try:
-            elem = elem[part]
-        except (KeyError, TypeError):
-            return None
-    return elem
+def category_str_to_items(cstr):
+    return list(filter(lambda i: bool(i),
+        [i.strip() for i in cstr.split(CATEGORY_SEPARATOR)]
+    ))
+
+def category_str_to_url(cstr):
+    category_items = category_str_to_items(cstr)
+    return ('/'.join(map(slugify, category_items))) + '/'
 
 
-def get_from_path(_dict, path, separator):
-    parts = [part.strip() for part in path.split(separator)]
-    return get_from_parts(_dict, parts)
-
-
-def get_sections(csv_lines, category_separator=DEFAULT_CATEGORY_SEPARATOR):
+def get_categories(csv_lines):
     csv_reader = csv.reader(csv_lines)
 
-    sections = {"links": [], "sections": {}, "title": "root", "path": "/"}
+    categories = {"categories": {}}
 
     for line in csv_reader:
         title, url, desc, category_str, tayp, lang, source = line[:-1]
-        category_path = [part.strip() for part in category_str.split(
-                         category_separator)]
-        category_path = list(filter(lambda i: bool(i), category_path))
-        parent = sections
-        for level in range(len(category_path)):
-            if "sections" not in parent:
-                parent["sections"] = {}
+        category_items = category_str_to_items(category_str)
+        parent = categories
+        for level in range(len(category_items)):
+            if "categories" not in parent:
+                parent["categories"] = {}
             else:
-                parent['sections'] = dict(sorted(parent['sections'].items()))
-            section_title = category_path[level]
+                parent['categories'] = dict(sorted(parent['categories'].items()))
+            category_title = category_items[level]
 
-            if section_title not in parent["sections"]:
-                category_slug = slugify(category_str, separator='-',
-                        replacements=[['>', '_'],])
-                parent["sections"][section_title] = {
-                    "title": section_title,
-                    "category_str": category_str,
-                    "slug": category_slug
+            if category_title not in parent["categories"]:
+                category_url = ('/'.join(map(slugify, category_items))) + '/'
+                parent["categories"][category_title] = {
+                    "title": category_title,
+                    "category_url": category_url,
+                    "level": level
                 }
 
-            parent = parent["sections"][section_title]
+            parent = parent["categories"][category_title]
     
         if "links" not in parent:
             parent['links'] = []
 
-        parent['links'].append({'title': title, 'url': url, 'desc': desc,
-                                'tayp': tayp, 'lang': lang,
-                                'url_slug': slugify(url)})
+        link = {
+            'title': title, 'url': url, 'desc': desc,
+            'tayp': tayp, 'lang': lang,
+            'category_url': category_str_to_url(category_str),
+            'filename': slugify(url) + '.html'
+        }
+        print(link)
+        parent['links'].append(link)
+
     
-    return sections
+    return categories
 
 
 def get_latest_links(csv_lines):
@@ -74,9 +74,15 @@ def get_latest_links(csv_lines):
     for line in csv_reader:
         title, url, desc, category_str, tayp, lang, source, date_str = line
         date = datetime.datetime.strptime(date_str, "%d.%m.%Y").date()
-        links.append({"title": title, "url": url, "desc": desc,
-                      "category": category_str, "tayp": tayp, "lang": lang,
-                      "source": source, "date": date})
+        links.append({
+            "title": title, "url": url, "desc": desc,
+            "tayp": tayp, "lang": lang,
+            "category_url": category_str_to_url(category_str),
+            "category_level": len(category_str_to_items(category_str)),
+            "filename": slugify(url) + '.html',
+            "source": source, "date": date,
+
+        })
     return sorted(links, key=lambda i: i['date'])
 
 
@@ -84,26 +90,43 @@ def get_latest_links(csv_lines):
 URL = "https://docs.google.com/spreadsheets/u/3/d/" \
       "1mK5BycfvwvuPcekTKIMhPKtsRa0EXe-dGeQsvok5wz4/pub?output=csv"
 
-# URL = "https://docs.google.com/spreadsheets/d
-# /1hE29WWJQ_sFnelcfWms7gHm8PnpLh4bVY6mdSP1S8SM/export?format=csv"
-
 csv_lines = [
     l.decode("utf-8") for l in urllib.request.urlopen(URL).readlines()[1:]
 ]
 
-sections = get_sections(csv_lines, category_separator=">")
-import json
-print(json.dumps(sections))
-
+categories = get_categories(csv_lines)
 latest_links = get_latest_links(csv_lines)
-home = env.get_template('home.html.jinja2')
-link_detail = env.get_template('link_detail.html.jinja2')
 
+home_template = env.get_template('home.html.jinja2')
+link_template = env.get_template('link.html.jinja2')
+category_template = env.get_template('category.html.jinja2')
+
+
+def render_categories(sub_categories, level=0):
+    for key, category in sub_categories.items():
+        path = join('docs', category['category_url'])
+        try:
+            makedirs(path)
+        except FileExistsError:
+            pass
+        with open(join(path, 'index.html'), 'w') as file:
+            file.write(
+                category_template.render(
+                    current_category=category,
+                    categories=categories
+                )
+            )
+        if 'links' in category:
+            for link in category['links']:
+                with open(join(path, slugify(link['url'])) + '.html', 'w') as file:
+                    file.write(link_template.render(
+                        link=link, current_category=category, categories=categories))
+
+        if 'categories' in category:
+            render_categories(category['categories'], level=level+1)
+
+
+render_categories(categories["categories"])
 with open('docs/index.html', 'w')as file:
-    file.write(home.render(sections=sections, latest_links=latest_links))
-
-for link in latest_links:
-    with open(join('docs', slugify(link['url'])) + '.html', 'w') as file:
-        file.write(link_detail.render(link=link, sections=sections))
-
+    file.write(home_template.render(categories=categories, latest_links=latest_links))
 
