605ef8389fe5e82ca8d460571ff325ab578657ac
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2021-05-12 20:27:43 +0300

    Updated design, refactored renderer, more mobile friendly version

--- render.py
+++ render.py
@@ -1,28 +1,44 @@
 import csv
-import urllib.request
-from slugify import slugify
 import datetime
-from os.path import join
-from os import makedirs
+import urllib.request
+
+from copy import deepcopy
+from slugify import slugify
+from os.path import join, exists
+from os import makedirs as _makedirs
+from collections import defaultdict
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from openpyxl import load_workbook
-
 from tempfile import TemporaryFile, NamedTemporaryFile
 
-# TODO: Read these as parameters from shell.
-DOCUMENT_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSXBGnECx6IhFmmeTt6QKLvy3rOvtvmUVaHq_Ubo1mPzWaJu_AfykRrJlurwrd9Ade9S5t7N4Zo2Qpa/pubhtml"
 DOCUMENT_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSXBGnECx6IhFmmeTt6QKLvy3rOvtvmUVaHq_Ubo1mPzWaJu_AfykRrJlurwrd9Ade9S5t7N4Zo2Qpa/pub?output=xlsx"
+SITE_TITLE = "LINK SITE"
+SITE_DESCRIPTION = "LINK SITE DESCRIPTION"
 
-CATEGORY_SEPARATOR = ">"
-LINKS = "Bağlantılar"
-CATEGORIES = "Kategoriler"
+SEPERATOR = ">"
+
+WORKBOOK_LINKS_TITLE = "Bağlantılar"
+WORKBOOK_CATEGORIES_TITLE = "Kategoriler"
+
+CATEGORY_COL = 3
+
+LINK_COLUMNS = (
+    "title",
+    "url",
+    "desc",
+    "category_str",
+    "kind",
+    "lang",
+    "source",
+    "create_time",
+)
 
 env = Environment(
-    loader=FileSystemLoader('templates/'),
-    autoescape=select_autoescape(['html', 'xml']))
+    loader=FileSystemLoader("templates/"), autoescape=select_autoescape(["html", "xml"])
+)
+
 
 def get_lines(worksheet):
-    """Returns worksheet as list of lists"""
     result = []
     for idx, row in enumerate(worksheet.rows):
         if idx == 0:
@@ -31,148 +47,203 @@ def get_lines(worksheet):
     return result
 
 
-def category_str_to_breadcrumbs(category_string):
-    """
-    Converts category string to list.
-
-    >>> category_str_to_breadcrumbs("A > B > C")
-    ["a", "b", "c"]
-    """
+def get_category_parts(id_):
     return list(
-        filter(
-            lambda breadcrumb: bool(breadcrumb), [
-                breadcrumb.strip() for breadcrumb in
-                category_string.split(CATEGORY_SEPARATOR)
-            ]
-        )
+        filter(lambda part: bool(part), [part.strip() for part in id_.split(SEPERATOR)])
     )
 
 
-def category_str_to_url(category_string):
-    """
-    Converts category string to url.
-
-    >>> category_str_to_url("A > B > C")
-    /a/b/c/
-    """
-    category_breadcrumbs = category_str_to_breadcrumbs(category_string)
-    return ('/'.join(map(slugify, category_breadcrumbs))) + '/'
+def get_category_path(id_):
+    parts = get_category_parts(id_)
+    return ("/".join(map(slugify, parts))) + "/"
 
 
-def build_categorised_links(workbook):
+def get_category_root_path(id_):
+    return "../" * (get_category_depth(id_) + 1)
 
-    categories_info = {}
-    for line in get_lines(workbook[CATEGORIES]):
-        categories_info[line[0]] = {
-            'title': line[1] if len(line) >= 2 else None,
-            'desc': line[2] if len(line) >= 3 else None
-        }
 
-    categories = {"categories": {}}
+def get_category_depth(id_):
+    return id_.count(SEPERATOR)
 
-    for line in get_lines(workbook[LINKS]):
-        title, url, desc, category_str, kind, lang, source, create_time =\
-            line
-        category_items = category_str_to_breadcrumbs(category_str)
-        parent = categories
-        for level in range(len(category_items)):
 
-            if "categories" not in parent:
-                parent["categories"] = {}
-            else:
-                parent['categories'] = dict(sorted(parent['categories'].items()))
+def get_category_parent_id(id_):
+    parts = get_category_parts(id_)
+    return f" {SEPERATOR} ".join(parts[:-1]) if parts else None
 
-            category_title = category_items[level]
 
-            if category_str in categories_info and \
-                categories_info[category_str]['title'] is not None:
-                category_seo_title = categories_info[category_str]['title']
-            else:
-                category_seo_title = category_title
+def get_link_from_line(line):
+    link = {column: line[index] for index, column in enumerate(LINK_COLUMNS)}
+    link["file_path"] = (
+        get_category_path(line[CATEGORY_COL]) + slugify(link["url"]) + ".html"
+    )
+    return link
 
-            if category_str in categories_info and \
-                categories_info[category_str]['desc'] is not None:
-                category_seo_desc = categories_info[category_str]['desc']
-            else:
-                category_seo_desc = None
 
-            if category_title not in parent["categories"]:
-                category_url = ('/'.join(map(slugify, category_items))) + '/'
-                parent["categories"][category_title] = {
-                    "title": category_title,
-                    "seo_title": category_seo_title,
-                    "seo_desc": category_seo_desc,
-                    "category_url": category_url,
-                    "level": level
-                }
+def get_links_by_category(lines):
+    result = defaultdict(list)
+    for line in lines:
+        id_ = line[CATEGORY_COL]
+        link = get_link_from_line(line)
+        result[id_].append(link)
+    return result
 
-            parent = parent["categories"][category_title]
-    
-        if "links" not in parent:
-            parent['links'] = []
 
-        link = {
-            'title': title, 'url': url, 'desc': desc,
-            'kind': kind, 'lang': lang, "create_time": create_time,
-            'category_url': category_str_to_url(category_str),
-            'filename': slugify(url) + '.html'
-        }
-        parent['links'].append(link)
+def create_category_paths(base_path, lines):
+    ids = get_links_by_category(lines).keys()
+    for id_ in ids:
+        path = join(base_path, get_category_path(id_))
+        makedirs(path)
+
+
+def get_category_overrides(lines):
+    overrides = {}
+    for line in lines:
+        override = {}
+        if len(line) > 1 and line[1] is not None:
+            override["title"] = line[1]
+        if len(line) > 2 and line[2] is not None:
+            override["desc"] = line[2]
+        overrides[line[0]] = override
+    return overrides
+
+
+def get_category_info(id_, overrides):
+    name = get_category_parts(id_)[-1]
+    result = {
+        "name": name,
+        "title": name,
+        "desc": None,
+        "parent": None,
+        "path": get_category_path(id_),
+        "children": [],
+    }
+    result.update(overrides.get(id_, {}))
+    return result
+
+
+def get_max_category_depth(links_page_lines):
+    max_depth = 0
+    for line in links_page_lines:
+        depth = get_category_depth(line[CATEGORY_COL])
+        max_depth = max(max_depth, depth)
+    return max_depth
+
+
+def get_categories(links_page_lines, categories_page_lines):
+    categories = {}
+    overrides = get_category_overrides(categories_page_lines)
+    max_depth = get_max_category_depth(links_page_lines)
+    for level in range(max_depth + 1):
+        for line in links_page_lines:
+            id_ = line[CATEGORY_COL]
+            depth = get_category_depth(id_)
+            if depth > level:
+                continue
+            category = get_category_info(id_, overrides)
+            categories[id_] = category
+            if depth == 0:
+                continue
+            parent_id = get_category_parent_id(id_)
+            categories[id_]["parent"] = parent_id
+            if parent_id not in categories:
+                categories[parent_id] = get_category_info(parent_id, overrides)
+            if id_ not in categories[parent_id]['children']:
+                categories[parent_id]["children"].append(id_)
     return categories
 
 
-def get_latest_links(workbook):
-
+def get_links_by_date(lines):
     links = []
-    for line in get_lines(workbook[LINKS]):
-        title, url, desc, category_str, tayp, lang, source, create_time = line
-        links.append({
-            "title": title, "url": url, "desc": desc,
-            "tayp": tayp, "lang": lang,
-            "category_url": category_str_to_url(category_str),
-            "category_level": len(category_str_to_breadcrumbs(category_str)),
-            "filename": slugify(url) + '.html',
-            "source": source, "create_time": create_time,
-        })
-    return sorted(links, key=lambda i: i['create_time'], reverse=True)
+    for line in lines:
+        links.append(get_link_from_line(line))
+    return sorted(links, key=lambda i: i["create_time"], reverse=True)
 
 
-temp_file = NamedTemporaryFile(suffix='.xlsx')
-temp_file.write(urllib.request.urlopen(DOCUMENT_URL).read())
-workbook = load_workbook(filename=temp_file.name, read_only=True)
-categories = build_categorised_links(workbook)
-latest_links = get_latest_links(workbook)[:20]
-
-home_template = env.get_template('home.html.jinja2')
-link_template = env.get_template('link.html.jinja2')
-category_template = env.get_template('category.html.jinja2')
-
-
-def render_categories(sub_categories, level=0):
-    for key, category in sub_categories.items():
-        path = join('docs', category['category_url'])
-        try:
-            makedirs(path)
-        except FileExistsError:
-            pass
-        with open(join(path, 'index.html'), 'w') as file:
+def render_categories(base_path, links_by_category, categories, template):
+    for id_, links in links_by_category.items():
+        category = categories[id_]
+        file_path = join(base_path, category["path"], "index.html")
+        root_path = get_category_root_path(id_)
+        with open(file_path, "w") as file:
             file.write(
-                category_template.render(
-                    current_category=category,
-                    categories=categories
+                template.render(
+                    links=links,
+                    root_path=root_path,
+                    category=category,
+                    categories=categories,
                 )
             )
-        if 'links' in category:
-            for link in category['links']:
-                with open(join(path, slugify(link['url'])) + '.html', 'w') as file:
-                    file.write(link_template.render(
-                        link=link, current_category=category, categories=categories))
+    for id_, category in categories.items():
+        if id_ in links_by_category:
+            continue
+        file_path = join(base_path, category["path"], "index.html")
+        root_path = get_category_root_path(id_)
+        with open(file_path, "w") as file:
+            file.write(
+                template.render(
+                    links=[],
+                    root_path=root_path,
+                    category=category,
+                    categories=categories,
+                )
+            )
 
-        if 'categories' in category:
-            render_categories(category['categories'], level=level+1)
+
+def render_links(base_path, links_by_category, template):
+    for id_, links in links_by_category.items():
+        for link in links:
+            file_path = join(base_path, link["file_path"])
+            with open(file_path, "w") as file:
+                file.write(
+                    template.render(link=link, root_path=get_category_root_path(id_))
+                )
 
 
-render_categories(categories["categories"])
-with open('docs/index.html', 'w')as file:
-    file.write(home_template.render(categories=categories, latest_links=latest_links))
+def render_home(base_path, link_page_lines, categories, template):
+    links = get_links_by_date(link_page_lines)
+    file_path = join(base_path, "index.html")
+    with open(file_path, "w") as file:
+        file.write(
+            template.render(
+                latest_links=links[:20], root_path="./", categories=categories
+            )
+        )
 
+
+def makedirs(path):
+    if exists(path):
+        return
+    try:
+        _makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def build(
+    document_url=None,
+    links_page_name_on_workbook="Bağlantılar",
+    categories_page_name_on_workbook="Kategoriler",
+    category_template_name="category.html.jinja2",
+    home_template_name="home.html.jinja2",
+    link_template_name="link.html.jinja2",
+    root_path="docs/",
+):
+
+    temp_file = NamedTemporaryFile(suffix=".xlsx")
+    temp_file.write(urllib.request.urlopen(DOCUMENT_URL).read())
+    workbook = load_workbook(filename=temp_file.name, read_only=True)
+    links_page_lines = get_lines(workbook[links_page_name_on_workbook])
+    categories_page_lines = get_lines(workbook[categories_page_name_on_workbook])
+    category_template = env.get_template(category_template_name)
+    link_template = env.get_template(link_template_name)
+    home_template = env.get_template(home_template_name)
+    links_by_category = get_links_by_category(links_page_lines)
+    categories = get_categories(links_page_lines, categories_page_lines)
+    create_category_paths(root_path, links_page_lines)
+    render_categories(root_path, links_by_category, categories, category_template)
+    render_links(root_path, links_by_category, link_template)
+    render_home(root_path, links_page_lines, categories, home_template)
+
+
+build()
