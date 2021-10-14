16716d7992467beeb9b9b0c016b9d1af14159946
Mirat Can Bayrak <mirat.bayrak@gmail.com>
2021-10-15 01:01:04 +0300

    Daily links

--- rebuild.py
+++ rebuild.py
@@ -360,7 +360,7 @@ def render_links(base_path, links_by_category, template):
                     )
                 )
             image_path = join(base_path, image_url)
-            if True or not exists(image_path):
+            if not exists(image_path):
                 safari.get("file://" + join(base_path, file_path))
                 safari.execute_script(cleaner_js)
                 safari.save_screenshot(join(base_path, image_url))
